7e86f36557a6e7b34c9088107045e7a4b35d3c18
Semjon Bibow <semjon.bibow@cgi.com>
2022-04-30 09:29:25 +0200

    [UI] Add basic menu
    This can be built on in the next commits once we know what options we
    expose to the user.

---
+++ src/Menus/Main.cs
@@ -0,0 +1,40 @@
+using Godot;
+using System;
+
+namespace Menus {
+public class Main : Control
+{
+	public override void _Ready()
+	{
+		GetNode<Button>("VBoxContainer/StartButton").GrabFocus();
+	}
+
+	private void _on_StartButton_pressed()
+	{
+		//GetTree().ChangeScene("res://Menus/OptionsMenu.tscn");
+	}
+
+	private void _on_OptionsButton_pressed()
+	{
+		GetNode<VBoxContainer>("VBoxContainer").Hide();
+
+		var options_scene = (PackedScene)ResourceLoader.Load("res://Menus/Options.tscn");
+		var options_scene_instance = (Control)options_scene.Instance();
+
+		GetTree().CurrentScene.AddChild(options_scene_instance);
+
+		options_scene_instance.Connect("BackButtonPressed", this, "_on_Options_BackButton_Signal");
+	}
+
+	private void _on_Options_BackButton_Signal()
+	{
+		GetNode<VBoxContainer>("VBoxContainer").Show();
+		GetNode<Button>("VBoxContainer/OptionsButton").GrabFocus();
+	}
+
+	private void _on_QuitButton_pressed()
+	{
+		GetTree().Quit();
+	}
+}
+}
---
+++ src/Menus/OptionsGraphics.cs
@@ -0,0 +1,25 @@
+using Godot;
+using System;
+
+public class OptionsGraphics : Control
+{
+	[Signal]
+	public delegate void BackButtonPressed();
+
+	public override void _Ready()
+	{
+		GetNode<Button>("VBoxContainer/FullScreenButton").GrabFocus();
+		GetNode<CheckButton>("VBoxContainer/FullScreenButton").Disabled = OS.WindowFullscreen;
+	}
+
+	private void _on_FullScreenButton_toggled(bool button_pressed)
+	{
+		OS.WindowFullscreen = button_pressed;
+	}
+
+	private void _on_BackButton_pressed()
+	{
+		QueueFree();
+		EmitSignal(nameof(BackButtonPressed));
+	}
+}
