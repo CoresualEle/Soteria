1e8c0f693822e2363da795df3e9ee0fb36b25f50
Frederic Oldenbüttel <frederic.oldenbuettel@yahoo.de>
2022-05-07 17:17:10 +0200

    Pull GetPathToNode up to Interface

--- src/Foundation/Contracts/INetworkGraph.cs
+++ src/Foundation/Contracts/INetworkGraph.cs
@@ -5,5 +5,14 @@ namespace Soteria.Foundation.Contracts
     public interface INetworkGraph
     {
         void RegisterConnection(INetworkConnection connectionToRegister);
+
+        /// <summary>
+        /// Finds the shortest path between two <see cref="INetworkNode" />--> on the graph using Dijkstra's algorithm.
+        /// See: https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm#Pseudocode
+        /// </summary>
+        /// <param name="source">Starting point of the path</param>
+        /// <param name="target">Endpoint of the path</param>
+        /// <returns>A list of the connections necessary to traverse the graph on the shortest path from source to target</returns>
+        IList<INetworkConnection> GetPathToNode(INetworkNode source, INetworkNode target);
     }
 }
