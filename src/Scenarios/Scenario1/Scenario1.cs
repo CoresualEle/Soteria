5f0521e236e8321c680e4c1f4a19ccccb19199a7
Semjon Bibow <semjon.bibow@cgi.com>
2022-05-18 20:14:49 +0200

    Use AutoLoad for GameVariables

--- src/Scenarios/Scenario1/Scenario1.cs
+++ src/Scenarios/Scenario1/Scenario1.cs
@@ -2,5 +2,13 @@ namespace Soteria.Scenarios.Scenario1
 {
     public class Scenario1 : ScenarioBase
     {
+        public override void _Ready()
+        {
+            base._Ready(); // First invoke base, because this is where the
+                           // gameVariables are loaded
+
+            this.gameVariables.Budget = 20000;
+            this.gameVariables.WorkSatisfaction = 0.8f;
+        }
     }
 }
--- src/Scenarios/ScenarioBase.cs
+++ src/Scenarios/ScenarioBase.cs
@@ -6,6 +6,13 @@ namespace Soteria.Scenarios
 {
     public class ScenarioBase : Node2D
     {
+        protected GameVariables gameVariables;
+
+        public override void _Ready()
+        {
+            this.gameVariables = GetNode<GameVariables>("/root/GameVariables");
+        }
+
         public override void _Input(InputEvent inputEvent)
         {
             if (inputEvent.IsActionPressed("ui_cancel"))
